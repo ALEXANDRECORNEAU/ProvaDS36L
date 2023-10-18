644d980fa16759556dacc52b00cec3a3a7db02af
ALEXANDRECORNEAU <106288769+ALEXANDRECORNEAU@users.noreply.github.com>
2023-10-18 00:26:09 +0000

    .

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header className="text-center  bg-gray-950	 rounded-t text-white md:flex justify-between ">
+            <h4>G</h4>
+            <nav>
+                <a className="hover:text-[#00e77f]  " href="#">
+                Any Query? &nbsp;
+                </a>
+            </nav>
+        </header>
+    )
+}
